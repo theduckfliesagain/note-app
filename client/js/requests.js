057738be46ec9f2605d98740aa398189d09c6b5a
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-03-31 10:45:35 +0100

    add inital client files

---
+++ client/js/requests.js
@@ -0,0 +1,10 @@
+const hostURL = "http://localhost:3000/posts";
+
+async function getPost(){
+    try {
+        console.log(window.location.pathname);
+        // const response = fetch(hostURL + )
+    } catch (error) {
+        
+    }
+}
